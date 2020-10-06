b80974072b1dbcd742c9f8479b1e9d8dca9b9ce9
gektor334 <62186772+gektor334@users.noreply.github.com>
2020-10-06 21:03:30 +0300

    add new file 'portfolio' and 'brand'

--- ds1/js/main.js
+++ ds1/js/main.js
@@ -17,7 +17,7 @@ var mySwiper = new Swiper('.swiper-container', {
   scrollbar: {
     el: '.swiper-scrollbar',
   },
-})
+});
 
 $(document).ready(function () {
   $('.client-slider').slick({
@@ -46,3 +46,33 @@ $(document).ready(function () {
     ]
   });
 });
+
+const portfoliobtn = document.querySelector('.portfolio__button');
+portfoliobtn.onclick = function (e) {
+  if (e.target.classList.contains('portfolio__btn')) {
+    for (let i = 0; i < portfoliobtn.children.length; i++) {
+      portfoliobtn.children[i].classList.remove('portfolio__btn--active');
+    }
+    e.target.classList.add('portfolio__btn--active');
+  }
+}
+
+let category1 = document.querySelector('.category-service');
+let category2 = document.querySelector('.category-grade');
+
+function categoryNoActive() {
+  category1.classList.remove('category-active');
+  category2.classList.remove('category-active');
+}
+function categoryActive1() {
+  category2.classList.remove('category-active');
+  category1.classList.add('category-active');
+}
+function categoryActive2() {
+  category1.classList.remove('category-active');
+  category2.classList.add('category-active');
+}
+
+document.querySelector('#portfolio1').onclick = categoryNoActive;
+document.querySelector('#portfolio2').onclick = categoryActive1;
+document.querySelector('#portfolio3').onclick = categoryActive2;
