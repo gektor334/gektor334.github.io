ee2c145b41f829d7b168812d92a4de57c238c74f
gektor334 <62186772+gektor334@users.noreply.github.com>
2020-12-23 15:21:33 +0300

    add animated

--- artonika_2021/js/main.js
+++ artonika_2021/js/main.js
@@ -18,31 +18,53 @@ window.onload = function () {
   }
 }
 
-let b1 = document.querySelector('.before-1');
-let b2 = document.querySelector('.before-2');
-let b3 = document.querySelector('.before-3');
-let b4 = document.querySelector('.before-4');
-let b5 = document.querySelector('.before-5');
+try {
+  let b1 = document.querySelector('.before-1');
+  let b2 = document.querySelector('.before-2');
+  let b3 = document.querySelector('.before-3');
+  let b4 = document.querySelector('.before-4');
+  let b5 = document.querySelector('.before-5');
 
-let top60 = '-60vh';
-let top80 = '-80vh';
-let top32 = '-32vh';
-let top30 = '-30vh';
-let top52 = '-52vh';
-let top40 = '-40vh';
-b1.style.setProperty('--sq-top', top60);
-b2.style.setProperty('--sq-top', top80);
-b3.style.setProperty('--sq-top', top80);
-b4.style.setProperty('--sq-top', top80);
-b5.style.setProperty('--sq-top', top80);
+  let top60 = '-60vh';
+  let top80 = '-80vh';
+  let top32 = '-32vh';
+  let top30 = '-30vh';
+  let top52 = '-52vh';
+  let top40 = '-40vh';
+  b1.style.setProperty('--sq-top', top60);
+  b2.style.setProperty('--sq-top', top80);
+  b3.style.setProperty('--sq-top', top80);
+  b4.style.setProperty('--sq-top', top80);
+  b5.style.setProperty('--sq-top', top80);
 
-document.querySelector('#btn1').onclick = function () {
-  setTimeout(() => b1.style.setProperty('--sq-top', top32), 100);
-  setTimeout(() => b2.style.setProperty('--sq-top', top30), 300);
-  setTimeout(() => b2.style.marginLeft = '0.5vh', 800);
-  setTimeout(() => b2.style.marginRight = '1.4vh', 800);
-  setTimeout(() => b3.style.setProperty('--sq-top', top52), 600);
-  setTimeout(() => b4.style.setProperty('--sq-top', top52), 900);
-  setTimeout(() => b5.style.setProperty('--sq-top', top40), 1200);
+  document.querySelector('#btn1').onclick = function () {
+    setTimeout(() => b1.style.setProperty('--sq-top', top32), 100);
+    setTimeout(() => b2.style.setProperty('--sq-top', top30), 300);
+    setTimeout(() => b2.style.marginLeft = '0.5vh', 800);
+    setTimeout(() => b2.style.marginRight = '1.4vh', 800);
+    setTimeout(() => b3.style.setProperty('--sq-top', top52), 600);
+    setTimeout(() => b4.style.setProperty('--sq-top', top52), 900);
+    setTimeout(() => b5.style.setProperty('--sq-top', top40), 1200);
+  }
+
 }
 
+catch { }
+
+try {
+  let animate = '5s linear infinite circle';
+  let s1 = document.querySelector('.btn');
+  let s2 = document.querySelector('.s2');
+  let s3 = document.querySelector('.button');
+  document.querySelector('.btn').onmouseover = function () {
+    s1.style.setProperty('--sq-animate', animate);
+    s2.style.setProperty('--sq-animate', animate);
+    s3.style.setProperty('--sq-animate', animate);
+  }
+  document.querySelector('.btn').onmouseout = function () {
+    s1.style = 'none';
+    s2.style = 'none';
+    s3.style = 'none';
+  }
+}
+catch { }
