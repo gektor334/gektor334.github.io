3c09ed98f6cf8cb4900e1e713318951a7a9a78d6
gektor334 <62186772+gektor334@users.noreply.github.com>
2021-01-24 16:30:08 +0300

    add popup

--- homo_web_conference/js/main.js
+++ homo_web_conference/js/main.js
@@ -12,3 +12,49 @@ for (let i = 0; i < btn.length; i++) {
   btn[i].onclick = up;
 }
 
+document.querySelector('.link-1').onclick = function () {
+  document.querySelector('.popup-1').style.display = 'flex';
+  document.querySelector('.hero').style.display = 'none';
+  document.querySelector('.questions').style.display = 'none';
+  document.querySelector('.content').style.display = 'none';
+  document.querySelector('.receive').style.display = 'none';
+  document.querySelector('.teacher').style.display = 'none';
+  document.querySelector('.footer').style.display = 'none';
+}
+
+document.querySelector('.link-2').onclick = function () {
+  document.querySelector('.popup-2').style.display = 'flex';
+  document.querySelector('.hero').style.display = 'none';
+  document.querySelector('.questions').style.display = 'none';
+  document.querySelector('.content').style.display = 'none';
+  document.querySelector('.receive').style.display = 'none';
+  document.querySelector('.teacher').style.display = 'none';
+  document.querySelector('.footer').style.display = 'none';
+}
+document.querySelector('.close').onclick = function () {
+  document.querySelector('.popup-1').style.display = 'none';
+  document.querySelector('.hero').style.display = 'flex';
+  document.querySelector('.questions').style.display = 'flex';
+  document.querySelector('.content').style.display = 'flex';
+  document.querySelector('.receive').style.display = 'flex';
+  document.querySelector('.teacher').style.display = 'flex';
+  document.querySelector('.footer').style.display = 'flex';
+}
+document.querySelector('.close-2').onclick = function () {
+  document.querySelector('.popup-2').style.display = 'none';
+  document.querySelector('.hero').style.display = 'flex';
+  document.querySelector('.questions').style.display = 'flex';
+  document.querySelector('.content').style.display = 'flex';
+  document.querySelector('.receive').style.display = 'flex';
+  document.querySelector('.teacher').style.display = 'flex';
+  document.querySelector('.footer').style.display = 'flex';
+}
+document.querySelector('.blackout-link').onclick = function () {
+  document.querySelector('.popup-1').style.display = 'flex';
+  document.querySelector('.hero').style.display = 'none';
+  document.querySelector('.questions').style.display = 'none';
+  document.querySelector('.content').style.display = 'none';
+  document.querySelector('.receive').style.display = 'none';
+  document.querySelector('.teacher').style.display = 'none';
+  document.querySelector('.footer').style.display = 'none';
+}
